d5bb9c4d79cac757360fe719bf1ad98d8f25258b
Deats <JDeaton26@gmail.com>
2025-06-12 22:05:13 -0700

    v1.0.0.1 - Single Port is working and I built one. Pairs with c# library version 1.0.0.1. Added Led, button, and get state command.

--- SCPS_Firmware/SCPS_Firmware.ino
+++ SCPS_Firmware/SCPS_Firmware.ino
@@ -1,30 +1,67 @@
-const int led = LED_BUILTIN;
+const int ledPin = 3;
 const int outputPin = 7;
+const int buttonPin = 5;
+const String EOM = "SCPS_EOM";
+const unsigned long debounceDelay = 50;
+String commandReceived = "";
+bool buttonLastState = HIGH;  // high = true = 1;
+unsigned long lastDebounceTime = 0;
+bool isDebouncing = false;
+bool powerState = false;
 
 void setup() {
   pinMode(outputPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
+  bool buttonState = digitalRead(buttonPin);
+  if (buttonState != buttonLastState && !isDebouncing){
+    isDebouncing = true;
+    lastDebounceTime = millis();
+  }
+  if (isDebouncing && (millis() - lastDebounceTime) > debounceDelay) {
+    if (buttonState == LOW && buttonLastState == HIGH) {
+      powerState = !powerState;
+      digitalWrite(outputPin, powerState);
+      digitalWrite(ledPin, powerState);
+    }
+    isDebouncing = false;
+    buttonLastState = buttonState;
+  }
+  
   if (Serial.available()){
-    String command = Serial.readStringUntil('\n');
-    command.trim();
-    
-    if (command == "AMICONNECTED?"){
-      Serial.println("YOURCONNECTED!");
-    }
-    else if (command == "ON"){
-      digitalWrite(outputPin, HIGH);
-      Serial.println("ON");
-    }
-    else if (command == "OFF"){
-      digitalWrite(outputPin, LOW);
-      Serial.println("OFF");
-    }
-    else{
-      Serial.println("Unknown command.");
+    char c = Serial.read();
+    commandReceived += c;
+    if (commandReceived.indexOf(EOM) != -1){
+      commandReceived.replace(EOM, "");
+      commandReceived.trim();
+      
+      if (commandReceived == "AMICONNECTED"){
+        Serial.println("YOURCONNECTED " + EOM);
+      }
+      else if (commandReceived == "ON"){
+        digitalWrite(outputPin, HIGH);
+        powerState = true;
+        digitalWrite(ledPin, powerState);
+        Serial.println("ON " + EOM);
+      }
+      else if (commandReceived == "OFF"){
+        digitalWrite(outputPin, LOW);
+        powerState = false;
+        digitalWrite(ledPin, powerState);
+        Serial.println("OFF " + EOM);
+      }
+      else if (commandReceived == "GET"){
+        String state = powerState ? "true" : "false";
+        Serial.println("GET " + state + " " + EOM);
+      }
+      else{
+        Serial.println("Unknown command. " + EOM);
+      }
+      commandReceived = "";
     }
   }
-
 }
